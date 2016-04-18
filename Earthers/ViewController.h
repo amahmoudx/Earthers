9f052d612f3ec479d650d0daed07cdf72f4e2a35
AhmedMahmoud <a.mahmoud@voktech.com>
2016-04-18 15:19:11 +0200

    Initial Commit

---
+++ Earthers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Earthers
+//
+//  Created by Ahmed Mahmoud on 4/18/16.
+//  Copyright © 2016 spaceAppCairo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Earthers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Earthers
+//
+//  Created by Ahmed Mahmoud on 4/18/16.
+//  Copyright © 2016 spaceAppCairo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
